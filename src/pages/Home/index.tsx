2211b734b994da1df2dfede3191cb97d53a0e1bc
jmamadeu <jmamadeu2000@gmail.com>
2020-08-02 17:06:05 +0100

    :zap: chore: Create no-responsive layout

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,121 +1,255 @@
 import React from 'react';
-import { ScrollView, Text, View } from 'react-native';
 
-import { AntDesign, Entypo, MaterialCommunityIcons } from '@expo/vector-icons';
+import { MaterialCommunityIcons, Feather } from '@expo/vector-icons';
+
+import { useTheme } from '../../hooks/Theme';
 
 import {
   Container,
-  Content,
   Header,
   LeftIcon,
   SwitchTheme,
   ContentFood,
+  SloganContainer,
   Slogan,
   SubSlogan,
   CategoryContainer,
   CategoryItem,
   CategoryImage,
+  FoodContainer,
+  FoodContentContainer,
+  FoodItem,
+  FoodImage,
+  FoodDescriptionContainer,
+  FoodTitle,
+  FoodSlogan,
+  PriceContainer,
+  PriceText,
 } from './styles';
-import { useTheme } from '../../hooks/Theme';
+import { Text, View } from 'react-native';
+
+const categoriesImage = [
+  { source: require('../../assets/hamburguer.png') },
+  { source: require('../../assets/pizza2.png') },
+  { source: require('../../assets/fast-food.png') },
+  { source: require('../../assets/hamburguer.png') },
+  { source: require('../../assets/fast-food.png') },
+  { source: require('../../assets/pizza2.png') },
+];
 
 const Home: React.FC = () => {
   const { currentTheme, handleChangeTheme } = useTheme();
 
   return (
     <Container style={{ backgroundColor: currentTheme.colors.background }}>
-      <Content>
-        <Header>
-          <LeftIcon>
-            <MaterialCommunityIcons
-              name='segment'
-              size={30}
-              color={currentTheme.colors.primary}
-            />
-          </LeftIcon>
-          <SwitchTheme onPress={handleChangeTheme}>
-            <MaterialCommunityIcons
-              name='theme-light-dark'
-              size={30}
-              color={currentTheme.colors.primary}
-            />
-          </SwitchTheme>
-        </Header>
-        <ContentFood>
+      <Header>
+        <LeftIcon>
+          <MaterialCommunityIcons
+            name='segment'
+            size={30}
+            color={currentTheme.colors.primary}
+          />
+        </LeftIcon>
+        <SwitchTheme onPress={handleChangeTheme}>
+          <MaterialCommunityIcons
+            name='theme-light-dark'
+            size={30}
+            color={currentTheme.colors.primary}
+          />
+        </SwitchTheme>
+      </Header>
+      <ContentFood>
+        <SloganContainer>
           <Slogan style={{ color: currentTheme.colors.primary }}>
-            From Texas With
+            Ghost Burger Restaurant
           </Slogan>
           <SubSlogan style={{ color: currentTheme.colors.text }}>
-            American Love
+            Hamburgers coloridos e gostosos!
           </SubSlogan>
-          <CategoryContainer>
+        </SloganContainer>
+        <CategoryContainer>
+          {categoriesImage.map((category, id) => (
             <CategoryItem
-              style={{ backgroundColor: currentTheme.colors.primary }}
+              key={id}
+              style={{
+                backgroundColor:
+                  currentTheme.title === 'light'
+                    ? currentTheme.colors.backgroundSecondary
+                    : currentTheme.colors.primary,
+              }}
             >
-              <CategoryImage />
+              <CategoryImage source={category.source} />
             </CategoryItem>
+          ))}
+        </CategoryContainer>
+        <FoodContainer>
+          <FoodContentContainer>
+            <FoodItem
+              style={{
+                backgroundColor:
+                  currentTheme.title === 'dark'
+                    ? currentTheme.colors.text
+                    : currentTheme.colors.backgroundSecondary,
+              }}
+            >
+              <View style={{ alignItems: 'center' }}>
+                <FoodImage
+                  resizeMode='stretch'
+                  source={require('../../assets/content/burger1.png')}
+                />
+              </View>
+              <FoodDescriptionContainer>
+                <FoodTitle style={{ color: currentTheme.colors.primary }}>
+                  Beef Burger
+                </FoodTitle>
+                <FoodSlogan
+                  style={{
+                    color:
+                      currentTheme.title === 'light'
+                        ? currentTheme.colors.text
+                        : currentTheme.colors.secondary,
+                  }}
+                >
+                  Spicy with garlick
+                </FoodSlogan>
+                <PriceContainer>
+                  <PriceText style={{ color: currentTheme.colors.primary }}>
+                    1000 Kz
+                  </PriceText>
+                  <Feather
+                    name='heart'
+                    color={currentTheme.colors.primary}
+                    size={24}
+                  />
+                </PriceContainer>
+              </FoodDescriptionContainer>
+            </FoodItem>
+            <FoodItem
+              style={{
+                backgroundColor:
+                  currentTheme.title === 'dark'
+                    ? currentTheme.colors.text
+                    : currentTheme.colors.backgroundSecondary,
+              }}
+            >
+              <View style={{ alignItems: 'center' }}>
+                <FoodImage
+                  resizeMode='stretch'
+                  source={require('../../assets/content/burger1.png')}
+                />
+              </View>
+              <FoodDescriptionContainer>
+                <FoodTitle style={{ color: currentTheme.colors.primary }}>
+                  Beef Burger
+                </FoodTitle>
+                <FoodSlogan
+                  style={{
+                    color:
+                      currentTheme.title === 'light'
+                        ? currentTheme.colors.text
+                        : currentTheme.colors.secondary,
+                  }}
+                >
+                  Spicy with garlick
+                </FoodSlogan>
+                <PriceContainer>
+                  <PriceText style={{ color: currentTheme.colors.primary }}>
+                    1000 Kz
+                  </PriceText>
+                  <Feather
+                    name='heart'
+                    color={currentTheme.colors.primary}
+                    size={24}
+                  />
+                </PriceContainer>
+              </FoodDescriptionContainer>
+            </FoodItem>
+          </FoodContentContainer>
+          <FoodContentContainer>
+            <FoodItem
+              style={{
+                backgroundColor:
+                  currentTheme.title === 'dark'
+                    ? currentTheme.colors.text
+                    : currentTheme.colors.backgroundSecondary,
+              }}
+            >
+              <View style={{ alignItems: 'center' }}>
+                <FoodImage
+                  resizeMode='stretch'
+                  source={require('../../assets/content/burger1.png')}
+                />
+              </View>
+              <FoodDescriptionContainer>
+                <FoodTitle style={{ color: currentTheme.colors.primary }}>
+                  Beef Burger
+                </FoodTitle>
+                <FoodSlogan
+                  style={{
+                    color:
+                      currentTheme.title === 'light'
+                        ? currentTheme.colors.text
+                        : currentTheme.colors.secondary,
+                  }}
+                >
+                  Spicy with garlick
+                </FoodSlogan>
+                <PriceContainer>
+                  <PriceText style={{ color: currentTheme.colors.primary }}>
+                    1000 Kz
+                  </PriceText>
+                  <Feather
+                    name='heart'
+                    color={currentTheme.colors.primary}
+                    size={24}
+                  />
+                </PriceContainer>
+              </FoodDescriptionContainer>
+            </FoodItem>
 
-            <CategoryItem
-              style={{ backgroundColor: currentTheme.colors.primary }}
+            <FoodItem
+              style={{
+                backgroundColor:
+                  currentTheme.title === 'dark'
+                    ? currentTheme.colors.text
+                    : currentTheme.colors.backgroundSecondary,
+              }}
             >
-              <CategoryImage />
-            </CategoryItem>
-
-            <CategoryItem
-              style={{ backgroundColor: currentTheme.colors.primary }}
-            >
-              <CategoryImage />
-            </CategoryItem>
-
-            <CategoryItem
-              style={{ backgroundColor: currentTheme.colors.primary }}
-            >
-              <CategoryImage />
-            </CategoryItem>
-
-            <CategoryItem
-              style={{ backgroundColor: currentTheme.colors.primary }}
-            >
-              <CategoryImage />
-            </CategoryItem>
-
-            <CategoryItem
-              style={{ backgroundColor: currentTheme.colors.primary }}
-            >
-              <CategoryImage />
-            </CategoryItem>
-
-            <CategoryItem
-              style={{ backgroundColor: currentTheme.colors.primary }}
-            >
-              <CategoryImage />
-            </CategoryItem>
-
-            <CategoryItem
-              style={{ backgroundColor: currentTheme.colors.primary }}
-            >
-              <CategoryImage />
-            </CategoryItem>
-
-            <CategoryItem
-              style={{ backgroundColor: currentTheme.colors.primary }}
-            >
-              <CategoryImage />
-            </CategoryItem>
-
-            <CategoryItem
-              style={{ backgroundColor: currentTheme.colors.primary }}
-            >
-              <CategoryImage />
-            </CategoryItem>
-
-            <CategoryItem
-              style={{ backgroundColor: currentTheme.colors.primary }}
-            >
-              <CategoryImage />
-            </CategoryItem>
-          </CategoryContainer>
-        </ContentFood>
-      </Content>
+              <View style={{ alignItems: 'center' }}>
+                <FoodImage
+                  resizeMode='stretch'
+                  source={require('../../assets/content/burger1.png')}
+                />
+              </View>
+              <FoodDescriptionContainer>
+                <FoodTitle style={{ color: currentTheme.colors.primary }}>
+                  Beef Burger
+                </FoodTitle>
+                <FoodSlogan
+                  style={{
+                    color:
+                      currentTheme.title === 'light'
+                        ? currentTheme.colors.text
+                        : currentTheme.colors.secondary,
+                  }}
+                >
+                  Spicy with garlick
+                </FoodSlogan>
+                <PriceContainer>
+                  <PriceText style={{ color: currentTheme.colors.primary }}>
+                    1000 Kz
+                  </PriceText>
+                  <Feather
+                    name='heart'
+                    color={currentTheme.colors.primary}
+                    size={24}
+                  />
+                </PriceContainer>
+              </FoodDescriptionContainer>
+            </FoodItem>
+          </FoodContentContainer>
+        </FoodContainer>
+      </ContentFood>
     </Container>
   );
 };
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -5,24 +5,28 @@ export const Container = styled.SafeAreaView`
   flex: 1;
 `;
 
-export const Content = styled.View`
-  flex: 1;
-  padding: 20px;
-  padding-top: ${Constants.statusBarHeight + 15}px;
-`;
 export const Header = styled.View`
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
-  margin-bottom: 25px;
+  margin-bottom: 18px;
+  padding: 20px;
+  padding-top: ${Constants.statusBarHeight - 5}px;
+  padding-bottom: 0;
 `;
 
 export const LeftIcon = styled.TouchableOpacity``;
 export const SwitchTheme = styled.TouchableOpacity``;
 
 export const ContentFood = styled.View`
-  flex: 0.8;
+  flex: 1;
 `;
+
+export const SloganContainer = styled.View`
+  flex-direction: column;
+  padding-left: 20px;
+`;
+
 export const Slogan = styled.Text`
   font-size: 26px;
   font-weight: bold;
@@ -34,14 +38,80 @@ export const SubSlogan = styled.Text`
 export const CategoryContainer = styled.ScrollView.attrs({
   horizontal: true,
   showsHorizontalScrollIndicator: false,
+  contentContainerStyle: {
+    paddingHorizontal: 20,
+  },
 })`
-  margin-top: 25px;
+  margin-top: 20px;
+  height: 70px;
+  max-height: 70px;
 `;
 
 export const CategoryItem = styled.TouchableOpacity`
-  width: 50px;
-  height: 50px;
-  border-radius: 25px;
+  width: 70px;
+  height: 70px;
+  border-radius: 35px;
   margin-right: 10px;
+  box-shadow: 0 1px;
 `;
-export const CategoryImage = styled.View``;
+export const CategoryImage = styled.Image`
+  width: 45px;
+  height: 45px;
+  align-self: center;
+  margin-top: 12px;
+`;
+
+export const FoodContainer = styled.ScrollView.attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyle: {
+    paddingTop: 30,
+  },
+})`
+  flex: 1;
+  margin-top: 20px;
+  padding-left: 20px;
+  padding-right: 20px;
+`;
+export const FoodContentContainer = styled.View`
+  flex-direction: row;
+  margin-bottom: 40px;
+`;
+export const FoodItem = styled.View`
+  flex: 1;
+  height: 150px;
+  margin-left: 10px;
+  border-radius: 16px;
+`;
+
+export const FoodImage = styled.Image`
+  width: 150px;
+  height: 150px;
+  margin-top: -60px;
+`;
+
+export const FoodDescriptionContainer = styled.View`
+  margin-top: -15px;
+  margin-left: 20px;
+  margin-bottom: 10px;
+`;
+
+export const FoodTitle = styled.Text`
+  font-weight: bold;
+  font-size: 19px;
+`;
+export const FoodSlogan = styled.Text`
+  font-size: 12px;
+`;
+
+export const PriceContainer = styled.View`
+  margin-top: 4px;
+  flex-direction: row;
+  justify-content: space-between;
+  padding-right: 20px;
+  align-items: center;
+`;
+
+export const PriceText = styled.Text`
+  font-weight: bold;
+  font-size: 19px;
+`;
